1e2ce7f061cf0030b50233f2d618beb294c7c557
Anderson Febrian <andersonfebrian@yahoo.com>
2021-07-16 21:51:07 +0800

    [Feature] Integrate Cloud Firestore

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:todo_list/services/todo_service.dart';
+import 'package:todo_list/models/todo.dart';
 
 class TodoAppHome extends StatefulWidget {
   const TodoAppHome({Key? key}) : super(key: key);
@@ -11,11 +13,11 @@ class _TodoAppHomeState extends State<TodoAppHome> {
   final _formKey = GlobalKey<FormState>();
 
   String temp = "";
+  List<Todo> data = [];
 
-  Map<int, dynamic> data = {
-    0: {"body": "Test", "isDone": false},
-    1: {"body": "Test", "isDone": false}
-  };
+  SnackBar snackBar({String? message}) {
+    return SnackBar(content: Text(message ?? ""));
+  }
 
   AlertDialog addTodoDialog(BuildContext context) {
     return AlertDialog(
@@ -29,7 +31,7 @@ class _TodoAppHomeState extends State<TodoAppHome> {
             });
           },
           validator: (value) {
-            if(value == null || value.isEmpty) {
+            if (value == null || value.isEmpty) {
               return "Please enter something!";
             }
             return null;
@@ -40,16 +42,18 @@ class _TodoAppHomeState extends State<TodoAppHome> {
         Container(
           child: TextButton(
               onPressed: () {
-                if(_formKey.currentState!.validate()){
+                if (_formKey.currentState!.validate()) {
                   setState(() {
-                    int id = data.keys.last + 1;
-                    data.addAll({
-                      id: {
-                        "body": temp,
-                        "isDone": false
+                    Todo tempData = new Todo(temp);
+                    data.add(tempData);
+                    TodoService.insertTodo(tempData).then((value) {
+                      if (value) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                            snackBar(
+                                message: "Successfully Added Todo!"));
                       }
+                      Navigator.pop(context);
                     });
-                    Navigator.pop(context);
                   });
                 }
               },
@@ -60,47 +64,135 @@ class _TodoAppHomeState extends State<TodoAppHome> {
     );
   }
 
+  AlertDialog deleteTodoDialog(BuildContext context, Todo item) {
+    return AlertDialog(
+      title: Text("Delete Todo?"),
+      actions: [
+        TextButton(
+            onPressed: () async {
+              await TodoService.deleteTodo(item).then((value) {
+                if (value) {
+                  setState(() {
+                    data.remove(item);
+                    ScaffoldMessenger.of(context).showSnackBar(
+                        snackBar(message: "Deleted Todo!"));
+                  });
+                  Navigator.pop(context);
+                }
+              });
+            },
+            child: Container(
+              margin: EdgeInsets.fromLTRB(0, 0, 20, 0),
+              child: Text("Confirm"),
+            )),
+      ],
+    );
+  }
+
+  AlertDialog updateTodoDialog(BuildContext context, Todo item) {
+    return AlertDialog(
+      title: Text("New Todo"),
+      content: Form(
+        key: _formKey,
+        child: TextFormField(
+          initialValue: item.body,
+          validator: (value) {
+            if (value == null || value.isEmpty) {
+              return "Please enter something!";
+            }
+            return null;
+          },
+          onChanged: (value){
+            setState(() {
+              temp = value;
+            });
+          },
+        ),
+      ),
+      actions: [
+        Container(
+          child: TextButton(
+            child: Text("Update"),
+            onPressed: () async {
+              if(_formKey.currentState!.validate()){
+                item.body = temp;
+                TodoService.updateTodo(item).then((value) {
+                  if(value){
+                    setState(() {
+                      data.where((element) => element == item);
+                    });
+                    ScaffoldMessenger.of(context).showSnackBar(snackBar(message: "Updated Todo!"));
+                  } else {
+                    ScaffoldMessenger.of(context).showSnackBar(snackBar(message: "Something Went Wrong..."));
+                  }
+                  Navigator.pop(context);
+                });
+              }
+            },
+          ),
+        ),
+      ],
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    TodoService.fetch().then((value) => data = value);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Todo List"),
         actions: [
-          Container(
-            margin: EdgeInsets.fromLTRB(0, 0, 20, 0),
-            child: GestureDetector(
-              child: Icon(Icons.more_vert),
-              onTap: () {
-                // TODO
-              },
-            ),
-          )
+          // Container(
+          //   margin: EdgeInsets.fromLTRB(0, 0, 20, 0),
+          //   child: GestureDetector(
+          //     child: Icon(Icons.more_vert),
+          //     onTap: () {
+          //       // TODO
+          //     },
+          //   ),
+          // ),
         ],
       ),
       body: Container(
-        child: ListView.builder(
-            itemCount: data.length,
-            itemBuilder: (context, index) {
-              return ListTile(
-                title: Text("${data[index]["body"]}"),
+          child: ListView(
+        children: [
+          for (var item in data)
+            GestureDetector(
+              child: ListTile(
+                title: Text(item.body),
+                leading: (item.isDone ?? false)
+                    ? Icon(Icons.check_box)
+                    : Icon(Icons.check_box_outline_blank),
                 trailing: GestureDetector(
+                  child: Icon(Icons.delete),
                   onTap: () {
-                    setState(() {
-                      data[index]["isDone"] = !data[index]["isDone"];
-                    });
+                    showDialog(
+                        context: context,
+                        builder: (context) => deleteTodoDialog(context, item));
                   },
-                  child: data[index]["isDone"]
-                      ? Icon(Icons.check_box)
-                      : Icon(Icons.check_box_outline_blank),
                 ),
-                onTap: () {
-                  setState(() {
-                    data[index]["isDone"] = !data[index]["isDone"];
-                  });
-                },
-              );
-            }),
-      ),
+              ),
+              onTap: () async {
+                setState(() {
+                  bool temp = item.isDone ?? false;
+                  item.isDone = !temp;
+                });
+                await TodoService.updateTodo(item)
+                    .then((value) => print(value));
+              },
+              onLongPress: () {
+                showDialog(
+                    context: context,
+                    builder: (context) => updateTodoDialog(context, item));
+              },
+            )
+        ],
+      )),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           showDialog(
--- lib/models/todo.g.dart
+++ lib/models/todo.g.dart
@@ -9,11 +9,21 @@ part of 'todo.dart';
 Todo _$TodoFromJson(Map<String, dynamic> json) {
   return Todo(
     json['body'] as String,
-    json['isDone'] as bool,
-  );
+  )
+    ..uuid = json['uuid'] as String?
+    ..isDone = json['isDone'] as bool?
+    ..createdAt = json['createdAt'] == null
+        ? null
+        : DateTime.parse(json['createdAt'] as String)
+    ..updatedAt = json['updatedAt'] == null
+        ? null
+        : DateTime.parse(json['updatedAt'] as String);
 }
 
 Map<String, dynamic> _$TodoToJson(Todo instance) => <String, dynamic>{
+      'uuid': instance.uuid,
       'body': instance.body,
       'isDone': instance.isDone,
+      'createdAt': instance.createdAt?.toIso8601String(),
+      'updatedAt': instance.updatedAt?.toIso8601String(),
     };
